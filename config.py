e9f288bf4848986b496bc28565a994dddeaf5259
Alejandro Sánchez Marcos <alejandrosanchezm@usal.es>
2021-09-18 17:41:43 +0200

    Second

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ class Config(object):
     TESTING = False
     SECRET_KEY = "OCML3BRawWEUeaxcuKHLpw"
     SESSION_COOKIE_SECURE = True
-    TMP_FILES = '/home/alex/Escritorio/Armando/develop/app/tmp/'
-    TEST_FILE ="/home/alex/Escritorio/Armando/develop/app/static/tests.json"
+    TMP_FILES = './app/tmp/'
+    TEST_FILE ="./app/static/tests.json"
     SEND_FILE_MAX_AGE_DEFAULT = 0
 
 class ProductionConfig(Config):
